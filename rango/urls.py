c2d192aa7b4bd1f91199d25507f3fa5dc9699855
anneli <2177212p@student.gla.ac.uk>
2017-02-07 17:08:03 +0000

    ch. 9.6

--- rango/urls.py
+++ rango/urls.py
@@ -6,8 +6,17 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^friends/$', views.friends, name='friends'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
-        views.show_category, name='show_category'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/', views.add_page, name='add_page'),
+        views.show_category,
+        name='show_category'),
+
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/',
+        views.add_page,
+        name='add_page'),
+
+    url(r'^register/$',
+        views.register,
+        name='register'),
 ]
 
